8a15fd0624aa88c7461283f095083715dd693afd
Hong Baojin <13809735212@163.com>
2019-05-28 18:45:11 +0800

    write to advantage

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,12 @@
 "use strict";
 
-$(function () {});
+$(function () {
+  /*课程表*/
+  // tab切换
+  $(".curriculum-tab-head .head-item").each(function (index) {
+    $(this).mouseenter(function () {
+      // console.log(index);
+      $(this).addClass("active").siblings().removeClass("active");
+    });
+  });
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 $(function () {
-
+  /*课程表*/
+  // tab切换
+  $(".curriculum-tab-head .head-item").each(function (index) {
+    $(this).mouseenter(function () {
+      // console.log(index);
+      $(this).addClass("active").siblings().removeClass("active");
+    })
+  });
 });
