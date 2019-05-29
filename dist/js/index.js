10a321fe6685fa7d2645b4855d8c73e8503ace67
Hong Baojin <13809735212@163.com>
2019-05-29 19:24:09 +0800

    write to footer

--- dist/js/index.js
+++ dist/js/index.js
@@ -5,8 +5,19 @@ $(function () {
   // tab切换
   $(".curriculum-tab-head .head-item").each(function (index) {
     $(this).mouseenter(function () {
-      // console.log(index);
       $(this).addClass("active").siblings().removeClass("active");
     });
+  });
+
+  /*教学优势*/
+  var tdArr = [];
+  // 往tdArr添加数据
+  $(".adv-col .td").each(function () {
+    tdArr.push($(this).outerHeight());
+  });
+  var tdMaxHeight = Math.max.apply(Math, tdArr);
+  // 把最高 高度赋值所有的.td
+  $(".adv-col .td").each(function () {
+    $(this).outerHeight(tdMaxHeight);
   });
 });
